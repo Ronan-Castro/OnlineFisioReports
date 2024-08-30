fc3dd2435ada25cb80bb80a6b3d759202bfef0f4
Ronan De Souza Castro <37640104+Ronan-Castro@users.noreply.github.com>
2024-08-30 17:42:31 -0300

    Correções

--- FisioReportsOnline/FisioOnlineReports/FisioOnlineReports.Client/Program.cs
+++ FisioReportsOnline/FisioOnlineReports/FisioOnlineReports.Client/Program.cs
@@ -1,9 +1,11 @@
+using FisioOnlineReports.Service;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<AmbienteService>();
+
 
 await builder.Build().RunAsync();
